d093ead21f31c8dce87c616290b4a7335f666250
marek-grvbowski <marek@grvbowski.com>
2024-10-30 21:14:30 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const referencesButton = document.getElementById('referencesButton');
+const calendarButton = document.getElementById('calendarButton');
+const contactButton = document.getElementById('contactButton');
+const carouselContainer = document.getElementById('carouselContainer');
+const calendarContainer = document.getElementById('calendarContainer
